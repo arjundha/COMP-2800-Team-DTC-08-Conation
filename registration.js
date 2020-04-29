017f88140800060b36fa9da6b2305b871b619cc3
Arjun <adhaliwal111@my.bcit.ca>
2020-04-28 17:25:21 -0700

    Temporary bug fix for passwords

--- registration.js
+++ registration.js
@@ -11,3 +11,27 @@ function checkPasswordMatch() {
         $("#divCheckPasswordMatchTrue").html("Passwords match.");
     }
 }
+
+function checkPasswordIsValid(input) {
+    if (input.value != document.getElementById('passwordInput').value) {
+        input.setCustomValidity('Passwords Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        input.setCustomValidity('');
+    }
+}
+
+function verifyPasswordsMatch(){
+    let password = document.getElementById("passwordInput").value;
+    let confirmPassword = document.getElementById("passwordConfirm").value;
+    if (password != confirmPassword){
+        alert("The passwords no not seem to match, please try again.");
+        password = "";
+        confirmPassword = "";
+        
+    }else{
+        return true
+    }
+}
+
+
