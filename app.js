7f7c11a8d5075caa9aa5468594827d8f1b79c667
Hudson McManus <hudson.mcmanus@gmail.com>
2020-05-01 14:42:41 -0700

    Rendered all requests for pages in app.js

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ const express = require("express")
 const app = express()
 const path = require('path');
 
-var fs = require('fs');
-var myCss = {
-  style: fs.readFileSync('public/main.css', 'utf8')
-};
-
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.render("index", {
-    title: "Landing Page",
-    myCss: myCss
-  });
+  res.render("index");
 })
 
 app.get('/login', (req, res) => {
 	res.render('login');
+});
+
+app.get('/registration', (req, res) => {
+	res.render('registration');
+});
+
+app.get('/customer_registration', (req, res) => {
+	res.render('customer_registration');
+});
+
+app.get('/business_registration', (req, res) => {
+	res.render('business_registration');
 });
 
 app.set('views', path.join(__dirname, 'views'));
