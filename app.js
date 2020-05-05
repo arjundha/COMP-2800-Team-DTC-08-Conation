34d7197cd199f2cf4655d06955c783dd0dd2df60
Arjun <adhaliwal111@my.bcit.ca>
2020-05-05 13:11:12 -0700

    Created layout.ejs and also reorganized folders

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
 const express = require("express")
 const app = express()
 const path = require('path');
+const ejsLayouts = require("express-ejs-layouts")
+
+app.use(ejsLayouts)
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("conation/index", { layout: 'layoutLoggedOut', title: 'Conation' });
 })
 
 app.get('/login', (req, res) => {
-	res.render('login');
+	res.render('conation/login', { layout: 'layoutLoggedOut', title: 'Log-In'  });
 });
 
 app.get('/registration', (req, res) => {
-	res.render('registration');
+	res.render('conation/registration', { layout: 'layoutLoggedOut', title: 'Registration'  });
 });
 
 app.get('/customer_registration', (req, res) => {
-	res.render('customer_registration');
+	res.render('conation/customer_registration', { layout: 'layoutLoggedOut', title: 'Customer Registration'  });
 });
 
 app.get('/business_registration', (req, res) => {
-	res.render('business_registration');
+	res.render('conation/business_registration', { layout: 'layoutLoggedOut', title: 'Business Registration'  });
 });
 
 app.get('/business', (req, res) => {
-	res.render('business');
+	res.render('conation/business'), { layout: 'layoutLoggedIn', title: 'SEND NAME OF BUSINESS HERE (USE REQ?)'  };
 });
 app.get('/update_business_info', (req, res) => {
-	res.render('update_business_info');
+	res.render('conation/update_business_info', { layout: 'layoutLoggedIn', title: 'Update Profile'  });
 });
 
 app.get('/main', (req, res) => {
-	res.render('main');
+	res.render('conation/main'), { layout: 'layoutLoggedIn', title: 'Conation'  };
 });
 
 app.set('views', path.join(__dirname, 'views'));
