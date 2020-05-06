a01a616b07a5fb27914560232947b527a30b9763
sarahesla <sarah.eslamdoust@gmail.com>
2020-05-06 16:37:11 -0700

    Working on updating DB via Form

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.get('/about', (req, res) => {
 app.get('/business', (req, res) => {
 	res.render('conation/business', { layout: 'layoutLoggedIn', title: 'SEND NAME OF BUSINESS HERE (USE REQ?)'  });
 });
+
 app.get('/update_business_info', (req, res) => {
 	res.render('conation/update_business_info', { layout: 'layoutLoggedIn', title: 'Update Profile'  });
 });
@@ -56,6 +57,42 @@ app.get('/update_business_info', (req, res) => {
 app.get('/main', (req, res) => {
 	res.render('conation/main', { layout: 'layoutLoggedIn', title: 'conation'  });
 });
+
+app.post('updateBusinessProfile', (req, res) => {
+	console.log("I did it");
+
+	pool.execute(`UPDATE business_owners 
+							SET first_name = ${req.body.firstName},
+								last_name = ${req.body.lastName},
+								email = ${req.body.email},
+								phone = ${req.body.phone}
+							WHERE username = ${req.body.username}`) // SHOULD BE ID BASED?
+		.then(data => res.json( {success : true} ))
+		.catch(error => res.json( {success : false} ));
+});
+
+app.post('updateBusinessPassword', (req, res) => {
+	pool.execute(`UPDATE business_owners 
+							SET password = ${req.body.password}
+							WHERE username = ${req.body.username}`) // SHOULD BE ID BASED AND PASSWORD NEEDS HASHING.
+		.then(data => res.json( {success : true} ))
+		.catch(error => res.json( {success : false} ));
+});
+
+app.post('updateBusinessInfo', (req, res) => {
+	pool.execute(`UPDATE businesses
+							SET address = ${req.body.address},
+								address2 = ${req.body.address2},
+								city = ${req.body.city},
+								province = ${req.body.province},
+								postal = ${req.body.postal},
+								category = ${req.body.category},
+								description = ${req.body.description}
+							WHERE business_id = ${req.body.username}`) // SHOULD BE ID BASED?
+		.then(data => res.json( {success : true} ))
+		.catch(error => res.json( {success : false} ));
+});
+
 
 app.listen(8080, function () {
   console.log("Server running. Visit: localhost:8080 in your browser 🚀");
