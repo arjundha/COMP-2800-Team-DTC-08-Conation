93f8d9d337ed2a51c482a5782984ed8a998a7194
Arjun <adhaliwal111@my.bcit.ca>
2020-05-11 18:07:55 -0700

    Commented out the animations

--- public/animations.js
+++ public/animations.js
@@ -1,53 +1,53 @@
-window.sr = ScrollReveal();
-      sr.reveal('.welcome', {
-        duration: 2000,
-        origin:'top'
-      });
-      sr.reveal('.welcome2', {
-        duration: 4000,
-        origin:'top',
-      });
-      sr.reveal('.enterLeft', {
-        duration: 2000,
-        origin:'left',
-        distance:'300px',
-        viewFactor: 0.4
-      });
-      sr.reveal('.enterRight', {
-        duration: 2000,
-        origin:'right',
-        distance:'300px',
-        viewFactor: 0.4
-      });
-      sr.reveal('.joinnow', {
-        duration: 2000,
-        origin:'bottom',
-        distance:'300px',
-        viewFactor: 0.2
-      });
-      sr.reveal('.team', {
-        duration: 2000,
-        origin:'left',
-        distance:'300px',
-        viewFactor: 0.2
-      });
-      sr.reveal('.teamnames', {
-        duration: 2000,
-        origin:'right',
-        distance:'300px',
-        viewFactor: 0.2
-      })
+// window.sr = ScrollReveal();
+//       sr.reveal('.welcome', {
+//         duration: 2000,
+//         origin:'top'
+//       });
+//       sr.reveal('.welcome2', {
+//         duration: 4000,
+//         origin:'top',
+//       });
+//       sr.reveal('.enterLeft', {
+//         duration: 2000,
+//         origin:'left',
+//         distance:'300px',
+//         viewFactor: 0.4
+//       });
+//       sr.reveal('.enterRight', {
+//         duration: 2000,
+//         origin:'right',
+//         distance:'300px',
+//         viewFactor: 0.4
+//       });
+//       sr.reveal('.joinnow', {
+//         duration: 2000,
+//         origin:'bottom',
+//         distance:'300px',
+//         viewFactor: 0.2
+//       });
+//       sr.reveal('.team', {
+//         duration: 2000,
+//         origin:'left',
+//         distance:'300px',
+//         viewFactor: 0.2
+//       });
+//       sr.reveal('.teamnames', {
+//         duration: 2000,
+//         origin:'right',
+//         distance:'300px',
+//         viewFactor: 0.2
+//       })
       
-      sr.reveal('.registerCustomer', {
-        duration: 3000,
-        origin:'left',
-        distance:'300px',
-        viewFactor: 0.2
-      })
+//       sr.reveal('.registerCustomer', {
+//         duration: 3000,
+//         origin:'left',
+//         distance:'300px',
+//         viewFactor: 0.2
+//       })
       
-      sr.reveal('.registerBusiness', {
-        duration: 3000,
-        origin:'right',
-        distance:'300px',
-        viewFactor: 0.2
-      });
+//       sr.reveal('.registerBusiness', {
+//         duration: 3000,
+//         origin:'right',
+//         distance:'300px',
+//         viewFactor: 0.2
+//       });
